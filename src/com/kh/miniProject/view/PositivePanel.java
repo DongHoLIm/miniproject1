ee6b3a421911fef906437716764e3cfeee89c5fb
LG PC <LG PC@LGPC>
2019-05-01 09:49:11 +0900

    update

--- src/com/kh/miniProject/view/PositivePanel.java
+++ src/com/kh/miniProject/view/PositivePanel.java
@@ -3,11 +3,20 @@ package com.kh.miniProject.view;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import com.kh.miniProject.model.vo.Day;
+
 public class PositivePanel extends JPanel{
 	
+	private JPanel PositivePanel;
+
 	public PositivePanel(JFrame start) {
-	      System.out.println("±‡¡§±‡¡§");
-	      
-	   }
+		PositivePanel = this;
+		System.out.println("±‡¡§±‡¡§");
+		Main.day2++;
+		Main.day++;
+		Main.dayBreak++;
+		new Day().day();
+		ChangePanel.ChangePanel(start, PositivePanel, new Main(start));
+	}
 
 }
