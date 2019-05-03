8724356431e88f767d2e9a32f572f6dfdb4e3072
eunju <eunju@DESKTOP-IE2PDJ4>
2019-05-03 09:19:33 +0900

    nooooo

--- src/com/kh/miniProject/view/Start.java
+++ src/com/kh/miniProject/view/Start.java
@@ -40,7 +40,7 @@ public class Start extends JFrame {
     
     	
     	
-    	/* File file = new File("sound/startBGM.wav");
+    	 File file = new File("sound/startBGM.wav");
          
          
          try {
@@ -55,8 +55,7 @@ public class Start extends JFrame {
          } catch(Exception e) {
              
              e.printStackTrace();
-         }*/
-        //占쏙옙占� Panel 占쏙옙占쏙옙 占쏙옙占쏙옙占쏙옙占쏙옙占쏙옙占쏙옙占� 占쏙옙占�      
+         }
     	startmain = new JPanel() {
             public void paintComponent(Graphics g) {
             	g.drawImage(img,0,0,960,730,this);
@@ -151,7 +150,7 @@ public class Start extends JFrame {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-//				/clip.stop();
+			clip.stop();
 				ChangePanel.StartChangePanle(start, startmain, new Prologue(start));
 				
 			}
